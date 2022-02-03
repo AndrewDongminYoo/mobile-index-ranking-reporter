7847c2d495c8061f89e28d40fa00a4312eaadd6f
dongminyoo <andrew@i-ve.kr>
2022-02-03 14:40:55 +0900

    modify: 랭킹 데이터 모델

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Timestamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Ranked(Timestamped):
+    RANK_TYPE = (
+        ("free", "무료 순위"),
+        ("paid", "유료 순위"),
+        ("gross", "매출 순위")
+    )
+    app_name = models.TextField(max_length=64)
+    icon_url = models.URLField(max_length=200)
+    market_appid = models.TextField(max_length=64)
+    package_name = models.TextField(max_length=64)
+    rank = models.IntegerField()
+    rank_type = models.CharField(choices=RANK_TYPE)
