3ec1b9076bda458f737f431c17e86f72b166042f
dongminyoo <andrew@i-ve.kr>
2022-02-03 13:48:31 +0900

    initial commit

---
+++ crawler/__init__.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Mobile_App(models.Model):
+    name = models.TextField()
+    publisher_name = models.TextField()
+    os
+    id
+    url
+    rating
+    price
+    version
+    imessage
+    publisher
+    publisher_address
+    publisher_country
+    feature_graphic
+    short_description
+    content_rating
+    rank
