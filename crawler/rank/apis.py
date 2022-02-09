e7aacd22d12c3da71732aeb7b12e94da87661839
dongminyoo <andrew@i-ve.kr>
2022-02-09 11:26:52 +0900

    refactoring: following & tracking api

--- crawler/rank/apis.py
+++ crawler/rank/apis.py
@@ -1,11 +1,15 @@
 from typing import List
 
+from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI
+from ninja.orm import create_schema
 
-from crawler.models import Ranked, Following
-from crawler.schemas import RankedSchema
+from crawler.models import Ranked, Following, TrackingApps
 
 api = NinjaAPI(title="Ninja")
+RankedSchema = create_schema(Ranked)
+FollowingSchema = create_schema(Following)
+TrackingSchema = create_schema(TrackingApps)
 
 
 @api.get("/search", response=RankedSchema)
@@ -18,6 +22,20 @@ def search(request, app_name):
     return ranked_app
 
 
-@api.post("/search", response=List[RankedSchema])
-def get_ranked_list(request):
-    pass
+@api.post("/following")
+def create_following(request, payload: FollowingSchema):
+    tracking = Following.objects.create(**payload.dict())
+    return {"id": tracking.id}
+
+
+@api.get("/tracking", response=List[TrackingSchema])
+def list_tracking(request):
+    qs = TrackingApps.objects.all()
+    return qs
+
+
+@api.delete("/following/{following_id}")
+def delete_following(request, following_id: int):
+    following = get_object_or_404(Following, id=following_id)
+    following.delete()
+    return {"success": True}
--- crawler/schemas.py
+++ crawler/schemas.py
@@ -1,9 +1,1 @@
-from ninja import ModelSchema
 
-from crawler.models import Ranked
-
-
-class RankedSchema(ModelSchema):
-    class Config:
-        model = Ranked
-        model_fields = "__all__"
