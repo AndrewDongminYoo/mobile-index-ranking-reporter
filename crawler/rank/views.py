9c9395025007dad73a5ea8a5f805c16048cc83cf
dongminyoo <andrew@i-ve.kr>
2022-02-09 11:54:03 +0900

    refactoring: groupby app

--- crawler/rank/views.py
+++ crawler/rank/views.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from django.db.models import Min
 from django.shortcuts import render
 from django.utils import timezone
 
@@ -12,4 +13,10 @@ def get_kst():
 
 def index(request):
     apps = TrackingApps.objects.filter(created_at__gte=get_kst() - timedelta(days=3))
-    return render(request, "statistic.html", {"apps": apps})
+    ratings = (
+        apps.values("created_at__date", "app_name", "market", "deal_type", "rank_type")
+            .annotate(rank=Min("rank"))
+            .values("created_at__date", "icon_url", "app_name", "market", "deal_type", "rank_type", "rank")
+            .order_by("-created_at__date")
+    )
+    return render(request, "statistic.html", {"apps": ratings})
