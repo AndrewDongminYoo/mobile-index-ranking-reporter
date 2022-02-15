1fca86a719b263e828d555490e342317892e5e65
dongminyoo <andrew@i-ve.kr>
2022-02-15 17:52:19 +0900

    model: export or import models with excel

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.db import models
+from import_export.admin import ImportExportMixin
 
 from crawler.forms import AppChoiceField
 from crawler.models import Following, App
@@ -11,27 +11,17 @@ from crawler.models import TrackingApps
 # Register your models here.
 
 
-@admin.register(Ranked)
-class Ranked(admin.ModelAdmin):
+class RankedAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["app_name", "package_name", "rank", "market", "deal_type", "rank_type", "created_at"]
     search_fields = ["app_name", "market_appid", "package_name"]
-
-    def get_action_choices(self, request, default_choices=models.BLANK_CHOICE_DASH):
-        print(default_choices)
-        return super().get_action_choices(request, default_choices)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "app":
             return AppChoiceField(queryset=App.objects.order_by("app_name").all())
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-    def get_search_results(self, request, queryset, search_term):
-        print("search")
-        return super().get_search_results(request, queryset, search_term)
 
-
-@admin.register(Following)
-class Following(admin.ModelAdmin):
+class FollowingAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["app_name", "created_at"]
     list_select_related = ["app"]
 
@@ -41,8 +31,7 @@ class Following(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-@admin.register(TrackingApps)
-class TrackingApps(admin.ModelAdmin):
+class TrackingAppsAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["app_name", "package_name", "market", "rank", "deal_type", "rank_type", "created_at"]
     search_fields = ["app_name", "package_name", ]
 
@@ -52,8 +41,7 @@ class TrackingApps(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-@admin.register(OneStoreDL)
-class OneStoreDL(admin.ModelAdmin):
+class OneStoreDLAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["app_name", "market_appid", "genre", "downloads", "volume", "released", "created_at"]
     search_fields = ["app_name", "market_appid", ]
 
@@ -61,3 +49,9 @@ class OneStoreDL(admin.ModelAdmin):
         if db_field.name == "app":
             return AppChoiceField(queryset=App.objects.order_by("app_name").all())
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Ranked, RankedAdmin)
+admin.site.register(Following, FollowingAdmin)
+admin.site.register(TrackingApps, TrackingAppsAdmin)
+admin.site.register(OneStoreDL, OneStoreDLAdmin)
