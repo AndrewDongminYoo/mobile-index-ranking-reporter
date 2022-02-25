15804ab13a93817e7b93d6e0a22c47edbbfa08d8
dongminyoo <andrew@i-ve.kr>
2022-02-25 14:59:06 +0900

    fix: index.html 초기화면 구성

--- crawler/rank/api2.py
+++ crawler/rank/api2.py
@@ -20,26 +20,39 @@ TrackingSchema = create_schema(TrackingApps)
 
 
 @api.get("/tracked", response=List[TrackingSchema], tags=["index"])
+@paginate(LimitOffsetPagination)
 def show_all_tracking_apps(request):
+    """추적 결과 전부"""
     print(request)
     return TrackingApps.objects.all()
 
 
+@api.get("/tracking/last", response=TrackingSchema, tags=["index"])
+def show_last_tracking_app(request):
+    """팔로잉 아이디로 추적 결과 리스트"""
+    following = request.GET.get("app")
+    return TrackingApps.objects.filter(following_id=following).order_by("-created_at").first()
+
+
 @api.get("/tracking", response=List[TrackingSchema], tags=["index"])
 @paginate(LimitOffsetPagination)
-def show_all_tracking_apps(request):
+def show_all_tracking_apps_with_following(request):
+    """팔로잉 아이디로 추적 결과 리스트"""
     following = request.GET.get("following")
-    return TrackingApps.objects.filter(following_id=following)
+    return TrackingApps.objects.filter(following_id=following).order_by("-created_at")
 
 
-@api.post("/search", response=ApplicationSchema, tags=["index"])
+@api.post("/search", response=List[ApplicationSchema], tags=["index"])
+@paginate(LimitOffsetPagination)
 def search_apps_with_query(request):
+    """앱 이름 혹은 마켓 아이디 검색하기"""
     query = request.POST.get('query')
     return App.objects.filter(Q(app_name__search=query) | Q(market_appid__search=query)).all()
 
 
 @api.post("/follow/new", response=FollowingSchema, tags=["index"])
 def new_follow_register(request):
+    """새로운 팔로우할 앱 등록하기"""
     app_data = request.POST
     follow = Following.objects.get_or_create(
         app_name=app_data.get('app_name'),
@@ -54,20 +67,33 @@ def new_follow_register(request):
 @api.get("/follow/list", response=List[FollowingSchema], tags=["index"])
 @paginate(LimitOffsetPagination)
 def load_all_following(request):
+    """팔로우 중인 앱 리스트"""
     return Following.objects.all()
 
 
-@api.get("/detail-1", response=List[OneStoreSchema], tags=["index"])
+@api.get("/downloads", response=List[OneStoreSchema], tags=["index"])
+@paginate(LimitOffsetPagination)
 def show_details_of_downloads(request):
+    """앱 기준으로 원스토어 다운로드수 리스트"""
     appid = request.GET.get("app")
     time3 = timezone.now() - timedelta(hours=30)
-    return OneStoreDL.objects\
+    OneStoreDL.objects\
         .filter(app_id=appid, created_at__gte=time3)\
         .values_list("downloads")
 
 
+@api.get("/down/last", response=OneStoreSchema, tags=["index"])
+def show_details_of_downloads_last(request):
+    """앱 기준으로 원스토어 다운로드수 리스트"""
+    appid = request.GET.get("app")
+    app = OneStoreDL.objects.filter(app_id=appid).order_by("-created_at").first()
+    return app if app else OneStoreSchema()
+
+
 @api.get("/detail-2", response=List[TrackingSchema], tags=["index"])
+@paginate(LimitOffsetPagination)
 def show_details_of_highest_ranks(request):
+    """앱 아이디 기준으로 일별 추적결과 리턴"""
     appid = request.GET.get("app")
     d_day = timezone.now() - timedelta(days=3)
     return TrackingApps.objects\
