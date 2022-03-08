03d473832eb69723928a5edb24c4648f50aa2c79
dongminyoo <andrew@i-ve.kr>
2022-03-08 14:32:42 +0900

    fix(debug): URL 형식 변경

--- crawler/rank/views.py
+++ crawler/rank/views.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from django.contrib.auth.decorators import login_required
 from django.core.handlers.wsgi import WSGIRequest
 from django.db.models import Min
 from django.shortcuts import render, redirect
@@ -7,7 +6,6 @@ from django.utils import timezone
 from crawler.models import TrackingApps, Ranked, OneStoreDL, TimeIndex, Following
 
 
-@login_required
 def index(request: WSGIRequest):
     return render(request, "index.html")
 
