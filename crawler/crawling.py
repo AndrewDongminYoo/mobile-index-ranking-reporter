3a22e1e1c476d951efa876e21a357e4a78ef26a8
dongminyoo <andrew@i-ve.kr>
2022-03-18 16:30:51 +0900

    feat(slack): 일간 순위 업데이트 로직 변경

--- crawler/crawling.py
+++ crawler/crawling.py
@@ -175,13 +175,13 @@ def crawl_app_store_rank(term: str, market: str, game_or_app: str):
                     rank_diff = tracking.rank - last_one.rank if last_one else 0
                     market_string = item.get_market_display()
                     if rank_diff < -1:
-                        print(f"순위 상승: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
-                        logger.debug(f"순위 상승: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
-                        post_to_slack(f"> 순위 상승: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        print(f"순위 상승🚀: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        logger.debug(f"순위 상승🚀: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        post_to_slack(f" 순위 상승🚀: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
                     if rank_diff > 1:
-                        print(f"순위 하락: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
-                        logger.debug(f"순위 하락: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
-                        post_to_slack(f"> 순위 하락: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        print(f"순위 하락🛬: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        logger.debug(f"순위 하락🛬: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        post_to_slack(f" 순위 하락🛬: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
 
 
 def following_one_crawl():
