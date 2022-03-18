862e343b8eebd7691ce18b471207bcc600190608
dongminyoo <andrew@i-ve.kr>
2022-03-18 14:13:13 +0900

    feat(slack): SLACK_WEB_HOOK_TEST

--- crawler/crawling.py
+++ crawler/crawling.py
@@ -115,7 +115,6 @@ def create_app(app_data: dict):
 
 
 def crawl_app_store_rank(term: str, market: str, game_or_app: str):
-    post_to_slack("`hello`")
     url = f'https://proxy-insight.mobileindex.com/chart/{term}'  # "realtime_rank_v2", "global_rank_v2"
     data = {
         "market": "all",  # "all", "google"
@@ -176,13 +175,13 @@ def crawl_app_store_rank(term: str, market: str, game_or_app: str):
                     rank_diff = tracking.rank - last_one.rank if last_one else 0
                     market_string = item.get_market_display()
                     if rank_diff < -1:
-                        print(f"순위 상승: {item.app_name} {market_string} {last_one.rank}위 -> {item.rank}위")
-                        logger.debug(f"순위 상승: {item.app_name} {market_string} {last_one.rank}위 -> {item.rank}위")
-                        post_to_slack(f"순위 상승: {item.app_name} {market_string} {last_one.rank}위 -> {item.rank}위")
+                        print(f"순위 상승: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        logger.debug(f"순위 상승: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        post_to_slack(f"> 순위 상승: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
                     if rank_diff > 1:
-                        print(f"순위 하락: {item.app_name} {market_string} {last_one.rank}위 -> {item.rank}위")
-                        logger.debug(f"순위 하락: {item.app_name} {market_string} {last_one.rank}위 -> {item.rank}위")
-                        post_to_slack(f"순위 하락: {item.app_name} {market_string} {last_one.rank}위 -> {item.rank}위")
+                        print(f"순위 하락: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        logger.debug(f"순위 하락: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
+                        post_to_slack(f"> 순위 하락: {item.app_name} {market_string} `{last_one.rank}위` -> `{item.rank}위`")
 
 
 def following_one_crawl():
