d7195d6ff22c2b91c06142356606e8bf4376ddf2
dongminyoo <andrew@i-ve.kr>
2022-04-04 16:16:27 +0900

    fix(cron): CELERY CELERY

--- ranker/settings.py
+++ ranker/settings.py
@@ -203,3 +203,6 @@ if not DEBUG:
             }
         },
     }
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
---
+++ ranker/tasks.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranker.settings')
+
+app = Celery('ranker')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+@app.task
+def add(x, y):
+    return x + y
