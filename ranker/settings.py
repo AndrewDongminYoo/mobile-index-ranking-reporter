ec9725c1a0f1795134f4b572a4d0b389393d11a9
dongminyoo <andrew@i-ve.kr>
2022-04-04 16:48:21 +0900

    fix(cron): CELERY CELERY

--- ranker/settings.py
+++ ranker/settings.py
@@ -198,3 +198,13 @@ if not DEBUG:
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Seoul'
+
+INSTALLED_APPS += (
+    'django_celery_beat',
+    'django_celery_results',
+)
--- ranker/tasks.py
+++ ranker/tasks.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranker.settings')
@@ -13,3 +14,35 @@ app.autodiscover_tasks()
 @app.task
 def add(x, y):
     return x + y
+
+
+@app.task(ignore_result=True)
+def ive_korea_internal_api():
+    url = "http://13.125.164.253/cron/update/following"
+    requests.post(url)
+
+
+@app.task(ignore_result=True)
+def following_one_crawl():
+    url = "http://13.125.164.253/v2/follow/list"
+    res = requests.get(url).json()
+    url = "http://13.125.164.253/cron/new/downloads"
+    for obj in res["items"]:
+        market_appid = obj["market_appid"]
+        if obj["market"] == "one":
+            print(market_appid)
+            requests.post(url, data={"market_appid": market_appid})
+
+
+@app.task(ignore_result=True)
+def crawl_app_store_hourly():
+    url = "http://13.125.164.253/cron/new/ranking"
+    requests.post(url, data={"market": "all"})
+    url = "http://13.125.164.253/cron/new/ranking/high"
+    requests.post(url)
+
+
+@app.task(ignore_result=True)
+def crawl_app_store_daily():
+    url = "http://13.125.164.253/cron/new/ranking"
+    requests.post(url, data={"market": "one"})
