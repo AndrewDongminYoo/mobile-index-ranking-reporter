29b4260154246144972424dc2d1fcabee9f7c184
dongminyoo <andrew@i-ve.kr>
2022-04-04 18:49:42 +0900

    fix(cron): init

--- ranker/tasks.py
+++ ranker/tasks.py
@@ -4,11 +4,8 @@ from celery import Celery
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranker.settings')
-
-app = Celery('ranker')
-
+app = Celery('ranker', broker="redis://localhost:6379/0")
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
 app.autodiscover_tasks()
 
 
