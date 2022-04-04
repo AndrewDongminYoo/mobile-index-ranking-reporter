fce6aa12a6a0fee9f3f723c880bccd8c02c6bcbb
dongminyoo <andrew@i-ve.kr>
2022-04-04 17:00:03 +0900

    fix(cron): return

---
+++ crawler/cron.py
@@ -0,0 +1,33 @@
+import requests
+
+
+def ive_korea_internal_api():
+    url = "http://13.125.164.253/cron/update/following"
+    requests.post(url)
+
+
+def following_one_crawl():
+    url = "http://13.125.164.253/v2/follow/list"
+    res = requests.get(url).json()
+    url = "http://13.125.164.253/cron/new/downloads"
+    for obj in res["items"]:
+        market_appid = obj["market_appid"]
+        if obj["market"] == "one":
+            print(market_appid)
+            requests.post(url, data={"market_appid": market_appid})
+
+
+def crawl_app_store_hourly():
+    url = "http://13.125.164.253/cron/new/ranking"
+    requests.post(url, data={"market": "all"})
+    url = "http://13.125.164.253/cron/new/ranking/high"
+    requests.post(url)
+
+
+def crawl_app_store_daily():
+    url = "http://13.125.164.253/cron/new/ranking"
+    requests.post(url, data={"market": "one"})
+
+
+if __name__ == '__main__':
+    following_one_crawl()
