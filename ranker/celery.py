ab67114f84605ebf36be91b057f0c4748a17fa54
dongminyoo <andrew@i-ve.kr>
2022-04-05 18:53:56 +0900

    fix(cron): django celery beat

--- ranker/celery.py
+++ ranker/celery.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranker.settings')
 app = Celery('ranker', broker="redis://localhost:6379/0")
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:ranker.settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
