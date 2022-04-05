e3a3161ecd995de94ea1e44d56980f85f78bfb5e
dongminyoo <andrew@i-ve.kr>
2022-04-05 19:01:57 +0900

    fix(cron): django celery beat

--- crawler/cron.py
+++ crawler/cron.py
@@ -1,7 +1,4 @@
 import requests
-from logging import getLogger
-
-logger = getLogger(__name__)
 
 
 def ive_korea_internal_api():
@@ -9,7 +6,6 @@ def ive_korea_internal_api():
         url = "http://13.125.164.253/cron/update/following"
         requests.post(url)
     except Exception as e:
-        logger.info(e)
         ive_korea_internal_api()
 
 
@@ -24,7 +20,6 @@ def following_one_crawl():
                 print(market_appid)
                 requests.post(url, data={"market_appid": market_appid})
         except Exception as e:
-            logger.info(e)
             following_one_crawl()
 
 
@@ -35,7 +30,6 @@ def crawl_app_store_hourly():
         url = "http://13.125.164.253/cron/new/ranking/high"
         requests.post(url)
     except Exception as e:
-        logger.info(e)
         crawl_app_store_hourly()
 
 
@@ -44,7 +38,6 @@ def crawl_app_store_daily():
         url = "http://13.125.164.253/cron/new/ranking"
         requests.post(url, data={"market": "one"})
     except Exception as e:
-        logger.info(e)
         crawl_app_store_daily()
 
 
