a5e964af365ab4e49e792bc7d1fd124ca5ff1e46
dongminyoo <andrew@i-ve.kr>
2022-04-06 10:14:34 +0900

    fix(cron): django celery beat

--- ranker/settings.py
+++ ranker/settings.py
@@ -61,8 +61,8 @@ CRONTAB_DJANGO_SETTINGS_MODULE = 'ranker.settings'
 CRONJOBS = [
     ('*/15 0-15 * * *', 'crawler.utils.get_following', ">> /home/ubuntu/ive.log"),
     ('0 * * * *', 'crawler.cron.crawl_app_store_hourly', ">> /home/ubuntu/0000.log"),
-    ('10 15 * * *', 'crawler.cron.following_one_crawl', '>> /home/ubuntu/0010.log'),
-    ('10 3 * * *', 'crawler.cron.crawl_app_store_daily', '>> /home/ubuntu/1210.log'),
+    ('10 15 * * *', 'crawler.cron.crawl_app_store_daily', '>> /home/ubuntu/0010.log'),
+    ('10 3 * * *', 'crawler.cron.following_one_crawl', '>> /home/ubuntu/1210.log'),
 ]
 
 ROOT_URLCONF = 'ranker.urls'
