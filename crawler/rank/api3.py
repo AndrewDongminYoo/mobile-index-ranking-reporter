c19ee1ea60f084ea7a0555cfff11535ecbfbe9af
dongminyoo <andrew@i-ve.kr>
2022-05-10 16:33:56 +0900

    fix(data fix): 새롭게 순위 진입 시 알림

--- crawler/rank/api3.py
+++ crawler/rank/api3.py
@@ -155,12 +155,15 @@ def new_ranking_app_from_data(request: WSGIRequest, market, game, term):
             )
             tracking.save()
             rank_diff = int(tracking.rank) - int(last.rank) if last else 0
+            is_new_rank = False if last else True
             market_str = item.get_market_display()
             LIVE = "*LIVE*" if item.market_appid in market_app_list else ""
+            text = ""
             if rank_diff < 0:
                 text = f" 순위 상승 {LIVE}🚀 {item.app_name} {market_str} `{last.rank}위` → `{item.rank}위`"
-                post_to_slack(text=text, following=following_app.id)
-            if rank_diff > 0:
+            elif rank_diff > 0:
                 text = f" 순위 하락 {LIVE}🛬 {item.app_name} {market_str} `{last.rank}위` → `{item.rank}위`"
-                post_to_slack(text=text, following=following_app.id)
+            elif is_new_rank is True:
+                text = f" 순위 진입! {LIVE}🤔 {item.app_name} {market_str} `{item.rank}위`"
+            post_to_slack(text=text, following=following_app.id) if text else None
         return item
